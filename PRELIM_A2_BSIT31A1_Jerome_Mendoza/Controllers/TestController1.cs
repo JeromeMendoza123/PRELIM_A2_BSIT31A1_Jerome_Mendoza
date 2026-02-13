51abcd327cb75da8467082c9dee4082b95d2a99f
JeromeMendoza123 <c1778-23@itmlyceumalabang.onmicrosoft.com>
2026-02-14 00:37:43 +0800

    added test controller

---
+++ PRELIM_A2_BSIT31A1_Jerome_Mendoza/Controllers/TestController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRELIM_A2_BSIT31A1_Jerome_Mendoza.Controllers
+{
+    public class TestController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
